f386b5ecf2f3fb1a17bfcb3fc331010d0e975536
L1nk01 <ecampospaulino@gmail.com>
2024-10-20 09:17:46 -0400

    emergency commit

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -8,38 +8,63 @@ import { ref } from 'vue';
 
 const isChecked = ref(false);
 const isOpen = ref(false);
+const isEditable = ref(false);
+const taskTitle = ref('Nombre de la tarea');
+const taskDescription = ref('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu tellus iaculis nisi condimentum fringilla. Nulla facilisi. Vivamus sed lorem quis lacus feugiat scelerisque at sed lectus. Proin sollicitudin bibendum risus, non vehicula nulla tristique sed. Phasellus consectetur maximus arcu et blandit');
 
-const checkButton = () => {
+const toggleCheck = () => {
   isChecked.value = !isChecked.value;
 }
 
-const openButton = () => {
+const toggleOpen = () => {
   isOpen.value = !isOpen.value;
 }
 
-const imgCheckChecked = check;
-const imgCheckBlank = check_blank;
+const toggleEdit = () => {
+  isEditable.value = !isEditable.value;
+}
+
+const handleEditAndOpen = () => {
+  toggleEdit();
+  if (!isOpen.value) toggleOpen();
+}
+
+const saveTaskInLocalStorage = () => {
+  localStorage.setItem('taskTitle', taskTitle.value);
+  localStorage.setItme('taskDescription', taskDescription.value);
+}
+
+const getTaskFromLocalStorage = () => {
+  taskTitle.value = localStorage.getItem('taskTitle') || 'Name';
+  taskDescription.value = localStorage.getItem('taskDescription') || 'Description';
+}
+
+const getTaskTitle = () => {
+  return taskTitle.value;
+}
+
+const getTaskDescription = () => {
+  return taskDescription.value;
+}
 </script>
 
 <template>
-  <div @click="openButton" class="task">
+  <div class="task">
     <div class="top">
       <div class="left-side">
-        <img @click="checkButton" :src="isChecked ? imgCheckChecked : imgCheckBlank" />
-        <span>Nombre de la tarea</span>
+        <img @click="toggleCheck" :src="isChecked ? check : check_blank" />
+        <span id="task-title" @click.stop :contenteditable="isEditable">{{ taskTitle }}</span>
       </div>
 
       <div class="right-side">
         <img :src="bin" />
-        <img :src="pencil" />
-        <img :class="{ 'task-opened': isOpen }" :src="right_arrow" />
+        <img @click="handleEditAndOpen" :src="pencil" />
+        <img @click="toggleOpen" :class="{ 'task-opened': isOpen }" :src="right_arrow" />
       </div>
     </div>
 
     <div :class="{ 'show': isOpen }">
-      <span :class="{ 'description': true, 'show': isOpen }">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu tellus iaculis nisi condimentum fringilla. Nulla facilisi. Vivamus sed lorem quis lacus feugiat scelerisque at sed lectus. Proin sollicitudin bibendum risus, non vehicula nulla tristique sed. Phasellus consectetur maximus arcu et blandit. Donec gravida lorem est,
-      </span>
+      <span id="task-description" @click.stop :class="{ 'description': true, 'show': isOpen }" :contenteditable="isEditable">{{ taskDescription }}</span>
     </div>
   </div>
 </template>
@@ -93,6 +118,16 @@ span {
   font-size: 1.2rem;
 }
 
+[contenteditable] {
+  border: none;
+  outline: none;
+}
+
+#task-description {
+  width: 100%;
+  height: 100%;
+}
+
 img.task-opened {
   transform: rotate(90deg);
   transition: all 300ms cubic-bezier(.23, 1, 0.32, 1);
@@ -103,7 +138,7 @@ img.task-opened {
 }
 
 div.show {
-  margin-top: 10px;
+  /* margin-top: 10px; */
 }
 
 span.show {
